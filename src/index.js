53a76aa78ec32eaae7e2f2dc42acbe483529b721
sonika <sonikak004@gmail.com>
2023-06-16 19:17:40 +0100

    Fix the UI and render few tasks using javascript

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import './style.css';
-import { renderTasks, addTask, deleteAllTasks } from './task.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const addTaskBtn = document.getElementById('addTaskBtn');
-  addTaskBtn.addEventListener('click', addTask);
+const tasks = [
+  { description: 'First task', completed: false, index: 1 },
+  { description: 'Second task', completed: true, index: 2 },
+  { description: 'Third task', completed: false, index: 3 },
+  { description: 'Fourth task', completed: true, index: 4 },
+  { description: 'Fifth task', completed: false, index: 5 },
+];
 
-  const deleteAllBtn = document.getElementById('deleteAllBtn');
-  deleteAllBtn.addEventListener('click', deleteAllTasks);
+function renderTasks() {
+  const taskList = document.getElementById('taskList');
+  taskList.innerHTML = '';
 
-  renderTasks();
-});
+  tasks.forEach((task) => {
+    const listItem = document.createElement('div');
+    listItem.classList.add('task');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.completed;
+    listItem.appendChild(checkbox);
+
+    const description = document.createElement('span');
+    description.textContent = task.description;
+    description.classList.add('font');
+    listItem.appendChild(description);
+
+    const options = document.createElement('div');
+    options.classList.add('options');
+
+    const dots = document.createElement('i');
+    dots.classList.add('fas', 'fa-ellipsis-v');
+    options.appendChild(dots);
+
+    listItem.appendChild(options);
+
+    taskList.appendChild(listItem);
+  });
+}
+
+renderTasks();
